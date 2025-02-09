3ce2c346936f74a10acac78dd6ae585a642c2492
sheba-am <saba.ameri1378@gmail.com>
2025-02-10 01:03:32 +0330

    adding tailwind

--- yt-clone/src/App.tsx
+++ yt-clone/src/App.tsx
@@ -3,20 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>youtube</h1>
     </div>
   );
 }
